b34a6388a420e700f1478509d5508b6504a3c994
banjun <banjun@gmail.com>
2021-01-14 23:38:53 +0900

    Initial Commit

---
+++ AudioAnalyzer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AudioAnalyzer
+//
+//  Created by BAN Jun on R 3/01/14.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AudioAnalyzer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AudioAnalyzer
+//
+//  Created by BAN Jun on R 3/01/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
